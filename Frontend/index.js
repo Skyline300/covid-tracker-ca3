d3d3316f17d2e4dcabef95bcabe3c0e47c0070be
Skyline300 <79264371+Skyline300@users.noreply.github.com>
2021-08-05 12:27:26 +0800

    deleting frontend for hosting

--- Frontend/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * @format
- */
-
-import {AppRegistry} from 'react-native';
-import App from './src/App';
-import {name as appName} from './app.json';
-import config from './appconfig.json';
-AppRegistry.registerComponent(appName, () => App(config));
